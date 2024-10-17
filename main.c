7442965d80373c1a90ee09dfec98003a2dd68f86
matteogirotti <matteogirotti10@gmail.com>
2024-10-17 16:50:58 +0200

    commit esercizio 4.18

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int num,i;
+    printf("inserisci un numero intero: ");
+    scanf("%d",&num);
+    printf("i divisori di %d sono: ", num);
+    for(int i=1;i<=num;i++) {
+        if(num%i==0) {
+
+        }
+    }
+    printf("%d",i);
+    return 0;
+}
